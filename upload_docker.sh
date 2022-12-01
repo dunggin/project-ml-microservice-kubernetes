7e3268de12ac87192aa534e88138a1cdf3440b32
Dunggin <dungho451@gmail.com>
2022-12-01 10:52:42 +0700

    Update docker scripts

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
@@ -6,6 +7,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=dunggin/project-ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +15,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath:latest
